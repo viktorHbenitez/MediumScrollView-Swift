9f0815abaaff3a9a7568f5f92c483024d7584f46
viktor hugo <victorHbenitez85@hotmail.com>
2018-09-22 19:24:54 -0500

    Init project

---
+++ MediumScrollView-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MediumScrollView-Swift
+//
+//  Created by Victor Hugo Benitez Bosques on 22/09/18.
+//  Copyright © 2018 Victor Hugo Benitez Bosques. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
